23b3a54ad89ceccb7af080b935b5436427065155
Boluwatife Owoade <owoade.adedamola@yahoo.com>
2020-07-18 17:11:38 +0100

    :+1: feat: SignIn page UI completed

--- src/components/Auth/SignIn.js
+++ src/components/Auth/SignIn.js
@@ -4,8 +4,27 @@ import './SignIn.css'
 
 const SignIn = () => {
   return (
-    <div>
-      hi
+    <div className="SignInWrapper">
+      <div className="SignIn">
+        <span className="SignIn__header">Welcome Back</span>
+        <span className="SignIn__header--secondary">Fill in your details to login to your account</span>
+        <form>
+          <div>
+            <label>Email Address</label>
+            <input type="email" required placeholder="mail@mail.com"/>
+          </div>
+          <div>
+            <label>Password</label>
+            <input type="password" required/>
+          </div>
+          <div>
+            <input className="checkbox" type="checkbox" />
+            <label>Remember me</label>
+          </div>
+          <button className="btn">Login to my Account</button>
+          <span>Forgot Passsword ?</span>
+        </form>
+      </div>
     </div>
   );
 };
