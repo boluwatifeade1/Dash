2a53126c0364fce607cb1ffd027bccbc3585e3a9
Boluwatife Owoade <owoade.adedamola@yahoo.com>
2020-07-18 15:39:23 +0100

    🥇initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import SiginIn from './components/Auth/SignIn'
+
+
+const App = () => {
+  return (
+    <SiginIn/>
+  )
+}
+
+export default App;
+
+
---
+++ src/components/Auth/SignIn.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SignIn.css'
+
+
+const SignIn = () => {
+  return (
+    <div>
+      hi
+    </div>
+  );
+};
+
+export default SignIn;
